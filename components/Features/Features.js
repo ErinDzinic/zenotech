ef118a3de2f8329b5a2edbebed96f1b805b4d5b5
Denis Tojaga <denis.tojaga@edu.fit.ba>
2022-04-17 11:00:25 +0200

    fix: features alignment

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -4,64 +4,61 @@ import {
     Heading,
     SimpleGrid,
     useColorModeValue as mode,
-  } from '@chakra-ui/react'
-  import * as React from 'react'
-  import { FaFileSignature, FaHandsHelping, FaHeadset } from 'react-icons/fa'
-  import { Feature } from './FeaturesDesign'
-  import Fade from 'react-reveal/Fade';
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { FaFileSignature, FaHandsHelping, FaHeadset, FaMobileAlt } from 'react-icons/fa';
+import { GiTeamUpgrade } from 'react-icons/gi';
+import { HiOutlineDesktopComputer } from 'react-icons/hi';
+import { Feature } from './FeaturesDesign'
+import Fade from 'react-reveal/Fade';
 
 
 //   ========== THIS IS COMPONENT FOR 2x3 column of ICONS BELOW OUR PARTNERS ================
-  
-  export default function Features(){
-    return (
-      
-          <Box bgColor={'white'} align='center'>
-          <Divider w='80%' my="20" opacity={1} />
-          <Fade top>
-          <Box align='center' pb='80px'> 
-              <Heading size={'4xl'} fontFamily='Rajdhani'>What do we offer</Heading> 
-            </Box>
-            </Fade>
-          <SimpleGrid
-            columns={{
-                base: 1,
-                md: 3,
-            }}
-            spacing={{
-                base: '10',
-                md: '2',
-            }}
-        >
 
-            {/* =============================================================
+export default function Features() {
+    return (
+
+        <Box bgColor={'white'} align='center'>
+            <Divider w='80%' my="20" opacity={1} />
+            <Fade top>
+                <Box align='center' pb='80px'>
+                    <Heading size={'4xl'} fontFamily='Rajdhani'>What do we offer</Heading>
+                </Box>
+            </Fade>
+            <SimpleGrid
+                paddingTop={'2rem'}
+                alignItems={'center'}
+                columns={{
+                    base: 1,
+                    md: 4,
+                }}
+                spacing={{
+                    base: '5',
+                    md: '5',
+                }}
+            >
+
+                {/* =============================================================
             KEEP EVERYTHING INSIDE OF FADE COMPONENT. ADD MORE JUST COPY PAStE , 
             FOR YOUR OWN PURPOSES CHANGE THE ICONS WHICEVER YOU FIT THE MOST 
             =================================================================*/}
 
-            <Fade bottom>
-            <Feature title="Share files" icon={<FaFileSignature />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            <Feature title="Connect with users" icon={<FaHeadset />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            <Feature title="Collaborate with partners" icon={<FaHandsHelping />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            <Feature title="Share files" icon={<FaFileSignature />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            <Feature title="Connect with users" icon={<FaHeadset />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            <Feature title="Collaborate with partners" icon={<FaHandsHelping />}>
-                Keep files and the messages about them together in channels.
-            </Feature>
-            </Fade>
-            <Divider my="10" opacity={0} />
-        </SimpleGrid>
+                <Fade bottom>
+                    <Feature title="Dedicated teams" icon={<GiTeamUpgrade />}>
+                        Hire our consultant who will give you the best services.
+                    </Feature>
+                    <Feature title="App development" icon={<FaMobileAlt />}>
+                        We create innovative app with modern design.
+                    </Feature>
+                    <Feature title="Collaborate with partners" icon={<FaHandsHelping />}>
+                        Collaborate with our partners and share your ideas.
+                    </Feature>
+                    <Feature title="Software development" icon={<HiOutlineDesktopComputer />}>
+                        Building software solutions. We integrate existing systems or we build them from scratch.
+                    </Feature>
+                </Fade>
+                <Divider my="10" opacity={0} />
+            </SimpleGrid>
         </Box>
     )
-  }
-  
+}
